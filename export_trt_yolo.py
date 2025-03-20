bbaa78d84fee6167d9423ff01adb11c1440332e3
mllightitup <43480503+mllightitup@users.noreply.github.com>
2025-03-21 00:12:16 +0300

    Update export_trt_yolo.py

--- export_trt_yolo.py
+++ export_trt_yolo.py
@@ -5,16 +5,3 @@ model = RTDETR("detector_weights/rtdetr-x.pt")
 model.export(
     format="engine", imgsz=640, dynamic=True, verbose=False, batch=8, half=True
 )
-
-# results = model.track(
-#     source=r"F:\shanghaitech\training\videos\08_016.avi",
-#     stream=True,
-#     show=True,
-#     classes=[0],
-#     line_width=1,
-#     half=True,
-#     tracker="bytetrack.yaml",
-# )
-#
-# for result in results:
-#     pass
