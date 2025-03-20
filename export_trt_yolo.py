88f360b58cdd4da008d7489cabf7f0d4e65f26c3
mllightitup <43480503+mllightitup@users.noreply.github.com>
2025-03-21 00:12:25 +0300

    Update export_trt_yolo.py

--- export_trt_yolo.py
+++ export_trt_yolo.py
@@ -1,6 +1,5 @@
 from ultralytics import RTDETR
 
-# model = RTDETR('detector_weights/rtdetr-x.engine')
 model = RTDETR("detector_weights/rtdetr-x.pt")
 model.export(
     format="engine", imgsz=640, dynamic=True, verbose=False, batch=8, half=True
